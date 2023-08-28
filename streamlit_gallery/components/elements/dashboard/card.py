c90afa34c56958bf3f7838d8d87d8ec989227731
takeumi <39755025+takeyuki1253@users.noreply.github.com>
2023-08-28 17:08:59 +0900

    Update card.py

--- streamlit_gallery/components/elements/dashboard/card.py
+++ streamlit_gallery/components/elements/dashboard/card.py
@@ -5,15 +5,13 @@ from .dashboard import Dashboard
 class Card(Dashboard.Item):
 
     DEFAULT_CONTENT = (
-        "This impressive paella is a perfect party dish and a fun meal to cook "
-        "together with your guests. Add 1 cup of frozen peas along with the mussels, "
-        "if you like."
+        "マグロの給餌量と給餌判断について "
     )
 
     def __call__(self, content):
         with mui.Card(key=self._key, sx={"display": "flex", "flexDirection": "column", "borderRadius": 3, "overflow": "hidden"}, elevation=1):
             mui.CardHeader(
-                title="Shrimp and Chorizo Paella",
+                title="生簀ごとのツナ給餌量",
                 subheader="September 14, 2016",
                 avatar=mui.Avatar("R", sx={"bgcolor": "red"}),
                 action=mui.IconButton(mui.icon.MoreVert),
@@ -22,8 +20,8 @@ class Card(Dashboard.Item):
             mui.CardMedia(
                 component="img",
                 height=194,
-                image="https://mui.com/static/images/cards/paella.jpg",
-                alt="Paella dish",
+                image="03-1.jpg",
+                alt="ikesu",
             )
 
             with mui.CardContent(sx={"flex": 1}):
