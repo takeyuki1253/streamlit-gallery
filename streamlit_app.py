a3acc00e7efeef0d113d249dbda1f36865210b86
takeumi <39755025+takeyuki1253@users.noreply.github.com>
2023-08-28 15:29:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,21 +7,21 @@ def main():
     page = page_group("p")
 
     with st.sidebar:
-        st.title("🎈 Okld's Gallery")
+        st.title("tsuna-test")
 
-        with st.expander("✨ APPS", True):
-            page.item("Streamlit gallery", apps.gallery, default=True)
+        # with st.expander("✨ APPS", True):
+        #     page.item("Streamlit gallery", apps.gallery, default=True)
 
         with st.expander("🧩 COMPONENTS", True):
-            page.item("Ace editor", components.ace_editor)
-            page.item("Disqus", components.disqus)
+            # page.item("Ace editor", components.ace_editor)
+            # page.item("Disqus", components.disqus)
             page.item("Elements⭐", components.elements)
-            page.item("Pandas profiling", components.pandas_profiling)
-            page.item("Quill editor", components.quill_editor)
-            page.item("React player", components.react_player)
+            # page.item("Pandas profiling", components.pandas_profiling)
+            # page.item("Quill editor", components.quill_editor)
+            # page.item("React player", components.react_player)
 
     page.show()
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Streamlit Gallery by Okld", page_icon="🎈", layout="wide")
+    st.set_page_config(page_title="Tsuna-test", page_icon="🎈", layout="wide")
     main()
