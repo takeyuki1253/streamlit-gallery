54b1854b416419fc48eea85661bdfaa46606d0a9
takeumi <39755025+takeyuki1253@users.noreply.github.com>
2023-08-29 17:40:30 +0900

    Update card.py

--- streamlit_gallery/components/elements/dashboard/card.py
+++ streamlit_gallery/components/elements/dashboard/card.py
@@ -28,7 +28,7 @@ class Card(Dashboard.Item):
             # )
 
             with mui.CardContent(sx={"flex": 1}):
-                with mui.Typography(content)
+                mui.Typography(content)
             # with mui.CardContent(sx={"flex": 1}):
             # # Here, we exit the mui context and use Streamlit's markdown function to render HTML
             #     st.markdown(content, unsafe_allow_html=True)
