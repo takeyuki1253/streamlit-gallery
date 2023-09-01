1490bd091d8436f68b07877a77a19739684eef52
takeumi <39755025+takeyuki1253@users.noreply.github.com>
2023-09-01 14:32:37 +0900

    Update card.py

--- streamlit_gallery/components/elements/dashboard/card.py
+++ streamlit_gallery/components/elements/dashboard/card.py
@@ -2,28 +2,23 @@ from streamlit_elements import mui
 import streamlit as st
 from .dashboard import Dashboard
 
+
 class Card(Dashboard.Item):
 
     DEFAULT_CONTENT = """
-        <h2>マグロの給餌量と給餌判断について</h2>
-        <p>日付：2023/8/29</p>
-        <p>本日の給餌可否： Yes</p>
-        <p>本日の給餌量： 100kg</p>
+        マグロの給餌量と給餌判断について
+        日付：2023/8/29 
+        本日の給餌可否： Yes
+        本日の給餌量： 100kg
     """
 
     def __call__(self, content):
-        with mui.Card(key=self._key, sx={"display": "flex", "flexDirection": "column", "borderRadius": 3, "overflow": "hidden"}, elevation=1):
-            mui.CardHeader(
-                title="生簀ごとのツナ給餌量",
-                subheader="September 14, 2016",
-                avatar=mui.Avatar("R", sx={"bgcolor": "red"}),
-                action=mui.IconButton(mui.icon.MoreVert),
-                className=self._draggable_class)
-            
-            with mui.CardContent(sx={"flex": 1}):
-                # Streamlit's markdown functionを使用してHTMLをレンダリング
-                st.markdown(content, unsafe_allow_html=True)
+        # カードのヘッダー
+        st.subheader("生簀ごとのツナ給餌量")
+        st.write("September 14, 2016")
 
+        # カードのコンテンツ
+        st.markdown(content, unsafe_allow_html=True)
 
 
 # class Card(Dashboard.Item):
